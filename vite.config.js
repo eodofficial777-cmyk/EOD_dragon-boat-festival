ca0d721c50f12c4d869f61b4f2886d052bcac481
eodofficial777-cmyk <eodofficial777@gmail.com>
2026-03-13 11:14:48 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // ★ 如果你的 GitHub repo 名稱不是 dragon-boat-festival，請改成你的 repo 名稱
 export default defineConfig({
   plugins: [react()],
-  base: '/dragon-boat-festival/',
+  base: '/EOD_dragon-boat-festival/',
 })
